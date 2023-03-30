e75ea992905e767fdd7a18ba01266509d9378e5e
blackprince001 <appiahboaduprince@gmail.com>
2023-03-30 21:11:07 +0000

    upd: remove unused imports

--- tests/api/test_checkbook_table_api.py
+++ tests/api/test_checkbook_table_api.py
@@ -1,21 +1,15 @@
 from manager.crud.checker import create_checkin, get_checkin
-import datetime
-
-from manager.schema.checkbook import CheckBookCreate
 
 
 def test_create_checkin(db, checkbook):
-
     checkin = create_checkin(
         db=db,
         checkin=checkbook
     )
-
     assert checkin.name == "blackprince"
 
 
 
 def test_get_worker(db, worker):
     checkin = get_checkin(db, name="blackprince")
-
     assert checkin.name == "blackprince"
